06dcd6e0cbd24a6238a9f6491cd971e9b66d9edc
Melnikov <approximatenumber@gmail.com>
2019-04-20 22:35:27 +0300

    spaces

--- capture.py
+++ capture.py
@@ -10,7 +10,7 @@ from xvfbwrapper import Xvfb
 
 
 WEBCAMS = {
-	"https://webcam.exdesign.ru/cds-polyustrovo-1": "webcam1.png",
+    "https://webcam.exdesign.ru/cds-polyustrovo-1": "webcam1.png",
     "https://webcam.exdesign.ru/cds-polyustrovo-2": "webcam2.png"}
 VIDEO_CLASSNAME = "container"
 
@@ -18,39 +18,39 @@ VIDEO_CLASSNAME = "container"
 def capture_photos():
 
 
-	options = Options()
-	options.headless = True
-	driver = webdriver.Chrome(options=options)
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
 
-	for url, pic in WEBCAMS.items():
-		print("%s: capturing..." % url)
-		driver.get(url)
-		sleep(5)
-		element = driver.find_element_by_class_name(VIDEO_CLASSNAME)
-		location = element.location
-		size = element.size
-		png = driver.get_screenshot_as_png()
+    for url, pic in WEBCAMS.items():
+        print("%s: capturing..." % url)
+        driver.get(url)
+        sleep(5)
+        element = driver.find_element_by_class_name(VIDEO_CLASSNAME)
+        location = element.location
+        size = element.size
+        png = driver.get_screenshot_as_png()
 
-		im = Image.open(BytesIO(png))
+        im = Image.open(BytesIO(png))
 
-		left = location['x']
-		top = location['y']
-		right = location['x'] + size['width']
-		if url == "https://webcam.exdesign.ru/cds-polyustrovo-1":
-			# the site lies about element size (850px)
-			bottom = 700
-		else:
-			bottom = location['y'] + size['height']
+        left = location['x']
+        top = location['y']
+        right = location['x'] + size['width']
+        if url == "https://webcam.exdesign.ru/cds-polyustrovo-1":
+            # the site lies about element size (850px)
+            bottom = 700
+        else:
+            bottom = location['y'] + size['height']
 
-		im = im.crop((left, top, right, bottom))
-		im.save(pic)
-		print("%s: captured as %s" % (url, pic))
-	driver.quit()
+        im = im.crop((left, top, right, bottom))
+        im.save(pic)
+        print("%s: captured as %s" % (url, pic))
+    driver.quit()
 
 
 def main():
-	with Xvfb(width=1920, height=1080) as xvfb:
-		capture_photos()
+    with Xvfb(width=1920, height=1080) as xvfb:
+        capture_photos()
 
 if __name__ == "__main__":
-	main()
+    main()
